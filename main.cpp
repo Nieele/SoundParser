bd580b0f9f80e410aa134944c64785b1737ec2f7
Nieelle <38255201+Nieele@users.noreply.github.com>
2023-07-14 01:00:34 +0300

    added LoaderJsonDataДобавьте файлы проекта.

--- main.cpp
+++ main.cpp
@@ -1,118 +1,44 @@
-﻿const int MIN_SERIAL_NUM_DEVICE = 0;
-const int MAX_SERIAL_NUM_DEVICE = 5;
-
+﻿#define NOMINMAX 
 #include <iostream>
-#include <fstream>
-#include <nlohmann/json.hpp>
-#include <xlnt/xlnt.hpp>
-#include <memory>
+#include <locale>
+#include <limits>
+#include <sstream>
+#include "LoaderJsonData.hpp"
 
-void createNamedTable(xlnt::worksheet ws, size_t countSheets) {
-	ws.title(std::to_string(countSheets));
-	ws.cell("A1").value("42,4");
-	ws.cell("B1").value("63");
-	ws.cell("C1").value("125");
-	ws.cell("D1").value("500");
-	ws.cell("E1").value("1000");
-	ws.cell("F1").value("1588");
-	ws.cell("G1").value("1990");
-	ws.cell("H1").value("2482");
-	ws.cell("I1").value("3040");
-	ws.cell("J1").value("3447");
-	ws.cell("K1").value("3747");
-	ws.cell("L1").value("4047,2");
-	ws.cell("M1").value("4395,4");
-	ws.cell("N1").value("4747,6");
-	ws.cell("O1").value("5047,7");
-	ws.cell("P1").value("5396,2");
-	ws.cell("Q1").value("5748");
-	ws.cell("R1").value("6048");
-	ws.cell("S1").value("6348");
-	ws.cell("T1").value("6600");
-}
-
-void parse(std::ifstream& inFile, int serialNum) 
+int main() 
 {
-	int hours_begin, minutes_begin;
-	int hours_end, minutes_end;
+	std::setlocale(LC_ALL, "");
 
-	char answer = 0;
-	size_t countSheets = 1;
-	
-	nlohmann::json jsonData = nlohmann::json::parse(inFile);
-	xlnt::workbook wb;
-	std::string serialNum_str = "0" + std::to_string(serialNum);
-
+	char answ;
 	do {
-		std::cout << "Enter a time range\n begin (h, m): ";
-		std::cin >> hours_begin >> minutes_begin;
-		std::cout << "end (h, m): ";
-		std::cin >> hours_end >> minutes_end;
+		std::cout << "1) Download data in one day" << std::endl;
+		std::cout << "2) Download data for a period of time" << std::endl;
+		std::cin >> answ;
 
-		auto ws = wb.create_sheet();
-		//createNamedTable(ws, countSheets);
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	} while (answ != '1' && answ != '2');
 
-		int column = 1, row = 2;
-
-		for (auto& el : jsonData) {
-			if (el.find("uName").value() == "Shok-MN" && el.find("serial").value() == serialNum_str) {
-
-				ws.cell(column + 20, row).value(std::string(el.find("Date").value()));
-
-				std::string valueData;
-				auto data = el.find("data").value();
-				for (size_t i = 0; i < 20; ++i) {
-					valueData = data.find("system_" + std::to_string(i)).value();
-					ws.cell(column + i, row).number_format(xlnt::number_format::number_comma_separated1());
-					ws.cell(column + i, row).value(std::stod(valueData));
-				}
-				++row;
-			}
-		}
-
+	if (answ == '1')
+	{
+		std::cout << "Enter date [DD MM YYYY]" << std::endl;
+		std::string timeString;
+		std::istringstream iss;
+		std::tm timeStruct;
 		do {
-			std::cout << "choose the next interval? [y/n]: ";
-			std::cin >> answer;
-		} while (answer != 'y' && answer != 'n');
+			std::getline(std::cin, timeString);
+			iss.str(timeString);
+			iss >> std::get_time(&timeStruct, "%d %m %y");
 
-		++countSheets;
-	} while (answer != 'n');
+			if (iss.fail())
+			{
+				std::cout << "Incorrect time format, try again" << std::endl;
+				iss.clear();
+			}
+		} while (iss.fail());
 
-	wb.save("example.xlsx");
-}
-
-bool checkCountArguments(int argc)
-{
-	if (argc != 3)
-	{
-		std::cout << "Too few arguments" << std::endl;
-		return false;
+		LoaderJsonData::load(timeStruct);
 	}
-	return true;
-}
-
-bool checkFileIsOpen(std::ifstream& inFile, const char filename[])
-{
-	if (!inFile.is_open())
-	{
-		std::cout << "Can't find the file (" << filename << ")" << std::endl;
-		return false;
-	}
-	return true;
-}
-
-int getSerialNumberDevice() {
-	std::cout << "Enter the serial number of the device: ";
-	int serialNum;
-	std::cin >> serialNum;
-
-	if (serialNum < MIN_SERIAL_NUM_DEVICE || serialNum > MAX_SERIAL_NUM_DEVICE) 
-	{
-		std::cout << "Incorrect serial number device" << std::endl;
-		return -1;
-	}
-	return serialNum;
-}
 
 // int argc, char* argv[]
 int main() 
